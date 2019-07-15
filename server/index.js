229d00ee5e0791632ee04e2b31e2948eb40210c2
esme <esme@github.com>
2019-07-15 15:36:12 -0700

    initial proxy setup with components

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const app = express();
+
+const port = 3000;
+
+app.use(morgan('dev'));
+app.use('/rooms/:listingid', express.static(path.resolve(__dirname, '../')));
+
+app.listen(port, () => {
+  console.log(`listening on ${port}`)
+});
