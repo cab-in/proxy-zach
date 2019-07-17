f87467ba242bb062326eaa3454d88bccc3b34d15
esme <esme@github.com>
2019-07-17 13:41:40 -0700

    add cdn for aws s3

--- server/index.js
+++ server/index.js
@@ -1,20 +1,12 @@
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
-const axios = require('axios');
-const app = express();
 
+const app = express();
 const port = 3000;
 
 app.use(morgan('dev'));
 app.use('/rooms/:listingid', express.static(path.resolve(__dirname, '../')));
-
-// Proxy way
-// app.get('bundle1.js', (req, res) => {
-//   axios.get('http://localhost:3001/bundle.js')
-//     .then(({data}) => res.sendFile(data))
-//     .catch(err => console.log('axios', err))
-// });
 
 app.listen(port, () => {
   console.log(`listening on ${port}`)
