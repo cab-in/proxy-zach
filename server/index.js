4e17be3454aa29e1546715dc3b8d64cfb3ebd4f5
esme <esme@github.com>
2019-07-17 00:16:44 -0700

    implement proxy server endpoints

--- server/index.js
+++ server/index.js
@@ -7,6 +7,19 @@ const port = 3000;
 
 app.use(morgan('dev'));
 app.use('/rooms/:listingid', express.static(path.resolve(__dirname, '../')));
+// app.use('/api/:listingid/booking', express.static(path.resolve(__dirname, '../')));
+
+app.all('/api/:listingid/booking', (req, res) => {
+  res.redirect(`http://localhost:3001/api/${req.params.listingid}/booking`);
+});
+
+app.all('/api/:listingid/gallery', (req, res) => {
+  res.redirect(`http://localhost:3003/api/${req.params.listingid}/booking`);
+});
+
+app.all('/api/:listingid/review', (req, res) => {
+  res.redirect(`http://localhost:3004/api/${req.params.listingid}/booking`);
+});
 
 app.listen(port, () => {
   console.log(`listening on ${port}`)
