8fc6756d90f219594cdd77368ace7935bf6bbbfa
esme <esme@github.com>
2019-07-17 10:44:21 -0700

    add comments for josh's method

--- server/index.js
+++ server/index.js
@@ -1,25 +1,20 @@
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
+const axios = require('axios');
 const app = express();
 
 const port = 3000;
 
 app.use(morgan('dev'));
 app.use('/rooms/:listingid', express.static(path.resolve(__dirname, '../')));
-// app.use('/api/:listingid/booking', express.static(path.resolve(__dirname, '../')));
 
-app.all('/api/:listingid/booking', (req, res) => {
-  res.redirect(`http://localhost:3001/api/${req.params.listingid}/booking`);
-});
-
-app.all('/api/:listingid/gallery', (req, res) => {
-  res.redirect(`http://localhost:3003/api/${req.params.listingid}/booking`);
-});
-
-app.all('/api/:listingid/review', (req, res) => {
-  res.redirect(`http://localhost:3004/api/${req.params.listingid}/booking`);
-});
+// Proxy way
+// app.get('bundle1.js', (req, res) => {
+//   axios.get('http://localhost:3001/bundle.js')
+//     .then(({data}) => res.sendFile(data))
+//     .catch(err => console.log('axios', err))
+// });
 
 app.listen(port, () => {
   console.log(`listening on ${port}`)
